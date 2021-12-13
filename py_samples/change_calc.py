4c8cde3d4457e6572effd96d3caac3d8dacfb199
im <poson_hemp@yahoo.co.jp>
2021-12-13 09:17:29 +0900

    make explicit type of each param & add def calc_change

--- py_samples/change_calc.py
+++ py_samples/change_calc.py
@@ -8,13 +8,25 @@ def main():
     input_amount = input('投入する金額(円): ')
     print_amount(input_amount)
 
+    calc_change(input_amount, price)
+
 
 def print_amount(yen):
     if not yen.isdecimal():
-        print(yen + ' は金額として無効です。整数を入力してください。')
+        print(str(yen) + ' は金額として無効です。整数を入力してください。')
         sys.exit()
     else:
-        print(yen + '円')
+        print(str(yen) + '円')
+
+
+def calc_change(input_amount, price):
+    change = int(input_amount) - int(price)
+
+    if change < 0:
+        print(str(abs(change)) + ' 円不足しています。')
+        sys.exit()
+    else:
+        print('お釣り: ' + str(change) + '円')
 
 
 if __name__ == "__main__":
